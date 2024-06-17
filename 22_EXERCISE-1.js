ae5e31df00e93a3100ed732bea08c6273b6863d6
RockyHandsome8 <101408698+RockyHandsome8@users.noreply.github.com>
2024-06-17 19:20:34 +0530

    xvbdg

--- 22_EXERCISE-1.js
+++ 22_EXERCISE-1.js
@@ -7,4 +7,24 @@ the score of the user. The program is expected to terminate
 once the number is guessed. Number should be between 1-100.
 */
 
+const { question } = require("readline-sync")
+
 let num = 69
+let guess
+let c = 0
+while (guess != num) {
+    let guess = question("Guess the number: ")
+    if (guess < num){
+        console.log(guess + " is less than the number, try again...")
+    }
+    else if (guess > num){
+        console.log(guess + " is greater than the number, try again...")
+    }
+    else if (guess == num){
+        console.log(guess + " is correct!!")
+        break
+    }
+    c += 1
+}
+
+console.log("\nAnd your score is " + (100 - c))
